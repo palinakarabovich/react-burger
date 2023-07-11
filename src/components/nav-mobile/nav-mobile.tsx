472a866a4123a7ad264a3ac6a7dd95e84ccc57f5
Palina Karabovich <polina.korobovich@yandex.by>
2023-07-11 14:09:34 +0200

    header position: fixed

--- src/components/nav-mobile/nav-mobile.tsx
+++ src/components/nav-mobile/nav-mobile.tsx
@@ -15,7 +15,15 @@ const NavMobile: React.FC<INavProps> = ({ location, menu, setMenu }) => {
   const { userSuccess } = useTypedSelector((store) => store.user);
 
   const handleSignOut = () => {
+    handleMenuClick();
     dispatch(logout());
+  }
+
+  const handleMenuClick = () => {
+    setProfileClicked(false);
+    if (setMenu) {
+      setMenu(false);
+    }
   }
 
   return (
@@ -23,9 +31,9 @@ const NavMobile: React.FC<INavProps> = ({ location, menu, setMenu }) => {
       {
         menu && setMenu && (
           <div className={navStyles.container}>
-            <div className={navStyles.icon_close} onClick={() => setMenu(false)}>
+            <div className={navStyles.icon_close} onClick={handleMenuClick}>
               <CloseIcon type='primary' />
-              </div>
+            </div>
             <h2 className={navStyles.title}>Menu</h2>
             <nav className={navStyles.navigation}>
               <p className={navStyles.profile} onClick={() => setProfileClicked(!isProfileClicked)}>
@@ -39,14 +47,14 @@ const NavMobile: React.FC<INavProps> = ({ location, menu, setMenu }) => {
               {
                 isProfileClicked && (
                   <div className={navStyles.profile_list}>
-                    <Link to='/react-burger/profile' className={navStyles.link} onClick={() => setMenu(false)}><NavItem description='Personal Information' active={!location.ordersHistory} /></Link>
-                    <Link to='/react-burger/profile/orders' className={navStyles.link} onClick={() => setMenu(false)}><NavItem description='Orders history' active={location.ordersHistory} /></Link>
+                    <Link to='/react-burger/profile' className={navStyles.link} onClick={handleMenuClick}><NavItem description='Personal Information' active={!location.ordersHistory} /></Link>
+                    <Link to='/react-burger/profile/orders' className={navStyles.link} onClick={handleMenuClick}><NavItem description='Orders history' active={location.ordersHistory} /></Link>
                     {userSuccess && <Link to='/react-burger' className={navStyles.link_profile} onClick={handleSignOut}><NavItem description='Sign out' /></Link>}
                   </div>
                 )
               }
-              <Link to='/react-burger' className={navStyles.link} onClick={() => setMenu(false)}><NavItem Icon={BurgerIcon} description='Burger Constructor' active={location.constructor} /></Link>
-              <Link to='/react-burger/feed' className={navStyles.link} onClick={() => setMenu(false)}><NavItem Icon={ListIcon} description='Order feed' active={location.orders} /></Link>
+              <Link to='/react-burger' className={navStyles.link} onClick={handleMenuClick}><NavItem Icon={BurgerIcon} description='Burger Constructor' active={location.constructor} /></Link>
+              <Link to='/react-burger/feed' className={navStyles.link} onClick={handleMenuClick}><NavItem Icon={ListIcon} description='Order feed' active={location.orders} /></Link>
             </nav>
           </div>
         )
